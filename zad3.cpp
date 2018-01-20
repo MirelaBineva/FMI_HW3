452ba0a5903f254916671a58f594687996d04cec
MirelaBineva <33381175+MirelaBineva@users.noreply.github.com>
2018-01-20 22:30:24 +0200

    Add files via upload

---
+++ zad3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    for (int i='A'; i<='Z'; i++)
+   {
+        cout << (char) i << " ASCII code " << i <<endl;
+   }
+
+    return 0;
+}
