d3101a93d8925885a7e89d309f291c457665a7ea
MirelaBineva <33381175+MirelaBineva@users.noreply.github.com>
2018-01-21 23:38:34 +0200

    Update zad9.cpp

--- zad9.cpp
+++ zad9.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 
 using namespace std;
-
+const int SIZE = 150;
+void print(char *str)
+{
+    for(int i=0; str[i] != '\n'; i++)
+    {
+        cout << str[i] << " ";
+    }
+    cout << endl;
+}
 int main()
 {
-
+    char str[SIZE];
+    //cin.getline(str, SIZE);
+    char c;
+    for (int i=0; i< SIZE; i++)
+    {
+        cin >> c;
+        if ( c== '\n')
+            break;
+        str[i] = c;
+    }
+    print(str);
 
 
 
