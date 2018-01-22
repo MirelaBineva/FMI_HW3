6d99f760229f0da126a9b2a693d0edbdf87359fe
MirelaBineva <33381175+MirelaBineva@users.noreply.github.com>
2018-01-22 14:03:23 +0200

    Update zad9.cpp

--- zad9.cpp
+++ zad9.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
+
 const int SIZE = 150;
-void print(char *str)
+/* void print(char *str)
 {
     for(int i=0; str[i] != '\n'; i++)
     {
         cout << str[i] << " ";
     }
     cout << endl;
-}
+} */
 int main()
 {
-    char str[SIZE];
+   /* char str[SIZE];
     //cin.getline(str, SIZE);
     char c;
     for (int i=0; i< SIZE; i++)
@@ -23,20 +24,29 @@ int main()
         str[i] = c;
     }
     print(str);
-
-    /* int el[256] = {0, };
+*/
+     char str[SIZE] = {0, };
     cin.getline(str, SIZE);
-    for (int i=0; str[i]!='\0'; i++)
-    {
-        el[(int)str[i]]++;
-    }
-    for (int i=0; i<256; i++)
-    {
-        if(el[i]>0)
-        cout << "character asci code: " << i << " : " << el[i] << endl;
-    }
-    cout << endl;
-    */
+
+    int asc[256];
+    for (int i = 0; i < 256; i++)
+	{
+		asc[i] = 0;
+	}
+
+	for (int i = 0; i < strlen(str); i++)
+	{
+		asc[(int)str[i]]++;
+	}
+
+    for (int i = 0; i < 256; i++)
+	{
+		if (asc[i] > 0)
+		{
+			cout <<(char)i<< " : " << asc[i] << endl;
+		}
+	}
+    
 
     return 0;
 }
